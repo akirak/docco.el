8208a7f91cccb2f4edbbf486913907c1f5a48e3d
Akira Komamura <akira.komamura@gmail.com>
2024-06-07 22:20:57 +0900

    Initial commit

---
+++ docco-ts.el
@@ -0,0 +1,75 @@
+;;; docco-ts.el --- Tree-sitter infrastructure for docco -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/akirak/docco.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This library provides infrastructure for editing comments based on
+;; the built-in support for tree-sitter.
+
+;;; Code:
+
+(require 'treesit)
+
+(cl-defun docco--ts-find (comment-node-type &key before line-comment)
+  (let ((node (docco--ts-find-ancestor-or-self (list comment-node-type before))))
+    ;; Find a location that begins a comment or is suitable for inserting a
+    ;; comment
+    (cond
+     ((equal (treesit-node-type node) comment-node-type)
+      (goto-char (treesit-node-start node)))
+     ((equal (treesit-node-type node) before)
+      (if-let (comment-node (docco--ts-find-previous-sibling node comment-node-type before))
+          (goto-char (treesit-node-start comment-node))
+        (goto-char (treesit-node-start node))))
+     (t
+      (user-error "not effective from this location")))
+    ;; Enter a comment body
+    (cond
+     ((looking-at (concat (regexp-quote line-comment) (rx (* blank))))
+      (goto-char (match-end 0)))
+     (t
+      (open-line 1)
+      (insert line-comment " ")))))
+
+(defun docco--ts-find-ancestor-or-self (node-types)
+  (let ((node (treesit-node-at (point))))
+    (catch 'find-ts-node
+      (while node
+        (when (member (treesit-node-type node) node-types)
+          (throw 'find-ts-node node))
+        (setq node (treesit-node-parent node))))))
+
+(defun docco--ts-find-previous-sibling (start goal-type not-type)
+  (let ((node start))
+    (catch 'find-prev-sibling
+      (while node
+        (setq node (treesit-node-prev-sibling node))
+        (when (equal (treesit-node-type node) goal-type)
+          (throw 'find-prev-sibling node))
+        (when (equal (treesit-node-type node) not-type)
+          (throw 'find-prev-sibling nil))))))
+
+(provide 'docco-ts)
+;;; docco-ts.el ends here
---
+++ docco.el
@@ -0,0 +1,83 @@
+;;; docco.el --- A framework for editing documentation comments -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "29.1"))
+;; Keywords: convenience
+;; URL: https://github.com/akirak/docco.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is the main library for docco, a framework for editing documentation
+;; comments.
+
+;;; Code:
+
+(require 'pcase)
+(require 'map)
+
+(defcustom docco-mode-alist
+  `((gleam-ts-mode
+     :treesit t
+     :treesit-patterns
+     ((function
+       "statement_comment"
+       :before "function"
+       :line-comment "///")
+      (module
+       "module-comment"
+       :before "source_file"
+       :line-comment "////"))))
+  ""
+  :type '(alist-get :key-type (symbol :tag "Major mode")
+                    :value-type plist))
+
+;;;; Infrastructure
+
+(defun docco--current-settings ()
+  (when-let (mode (apply #'derived-mode-p (mapcar #'car docco-mode-alist)))
+    (cdr (assq mode docco-mode-alist))))
+
+(defun docco--edit-comment (type)
+  (pcase-exhaustive (docco--current-settings)
+    ((and (map :treesit :treesit-patterns)
+          (guard treesit)
+          (let `(,_ ,comment-node-type . ,plist) (assq type treesit-patterns)))
+     (require 'treesit)
+     (apply #'docco--ts-find comment-node-type plist))))
+
+;;;;; Tree-sitter infrastructure
+
+;;;###autoload
+(defun docco-edit-module-comment ()
+  "Insert a documentation comment for the module containing the point."
+  (interactive)
+  (docco--edit-comment 'module))
+
+;;;###autoload
+(defun docco-edit-function-comment ()
+  "Insert a documentation comment for the function at point."
+  (interactive)
+  (docco--edit-comment 'function))
+
+(provide 'docco)
+;;; docco.el ends here
