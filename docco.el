e2ed89230e2799a1cdc99e573a460c4449845c25
Akira Komamura <akira.komamura@gmail.com>
2025-03-23 16:04:03 +0900

    fix: Add lexical_declaration node type for TypeScript

--- docco.el
+++ docco.el
@@ -100,7 +100,10 @@
      ((comment
        "comment"
        :key ?c
-       :before ("class_declaration" "export_statement" "type_alias_declaration"
+       :before ("class_declaration"
+                "export_statement"
+                "type_alias_declaration"
+                "lexical_declaration"
                 "function_declaration")
        :comment-start-regexp ,(rx "/**" (? (* blank) "\n" (* blank) "*") (+ blank))
        :skeleton (> "/**" n " * " _ n " */")))))
